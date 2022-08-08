20b5af8a904faba8856d654037a7a9f9ddb6933a
@1ss1222 <1ss1222@naver.com>
2022-08-08 22:52:39 +0900

    바코드 스캔 수정

--- app/src/main/java/com/example/barcode_20220802/ScanActivity.java
+++ app/src/main/java/com/example/barcode_20220802/ScanActivity.java
@@ -22,11 +22,6 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class ScanActivity extends AppCompatActivity {
 
-    /* QR code scanner 객체 */
-//    private IntentIntegrator qrScan;
-//    private static final int ZXING_CAMERA = 101; //ZXING 카메라 권한요청 ID
-//    private StandardObject standardObj = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,69 +48,25 @@ public class ScanActivity extends AppCompatActivity {
          etTyp.setText(barcodetype);
 
          ImageView img_barcode;
-         img_barcode = (ImageView)findViewById(R.id.img_barcode) ;
+         img_barcode = (ImageView)findViewById(R.id.img_barcode);
 
          MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
          final int WIDTH = 360;
          final int HEIGHT = 180;
 
          try {
+
              BitMatrix bitMatrix = multiFormatWriter.encode(barcodenum, BarcodeFormat.valueOf(barcodetype), WIDTH, HEIGHT);
              BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
              Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
              img_barcode.setImageBitmap(bitmap);
+
          } catch (Exception e) {
          }
-
-         CreateBarcode barcodeImg = new CreateBarcode();
-         barcodeImg.createBarcode(this.toString(),barcodetype,barcodenum);
 
 
         }
     }
 
-
-
-
-
-
-        //        /* QR code Scanner Setting */
-//        qrScan = new IntentIntegrator(this);
-//        qrScan.setPrompt("아래 띄울 문구");
-//        qrScan.initiateScan();
-//        qrScan.setCaptureActivity(EmptyActivity.class);
-//    }
-//
-//    /* Getting the Scan Results */
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//
-//        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-//
-//        if (result != null) {
-//            if (result.getContents() == null) {
-//                Log.v("qrcode :::::::::::", "no contents");
-//            } else { //QR코드, 내용 존재
-//                try {
-//                    /* QR 코드 내용*/
-//                    String temp = result.getContents();
-//
-//                    /* 로직
-//                     *
-//                     * 로직 끝 */
-//
-//                    Log.v("qrcode Contents :::::::::::", temp);
-//                    Toast.makeText(getApplicationContext(), result.getContents(), Toast.LENGTH_LONG).show();
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                    Log.v("Exception :::::::::::::", "QR code fail");
-//
-//                }
-//            }
-//        } else {
-//            super.onActivityResult(requestCode, resultCode, data);
-//        }
-//    }
 
 }
